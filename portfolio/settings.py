37a67a8e3abed38b59faf635751f5cfd231de720
shabna <136456238+shabnashabz@users.noreply.github.com>
2024-10-07 21:29:41 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,9 +118,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
